f10bc7e22a75298e64e5346a47c6b2f0b5d69347
Zeyneb Aboelnasr <if22b185@technikum.wien.at>
2023-05-08 00:15:06 +0200

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule
