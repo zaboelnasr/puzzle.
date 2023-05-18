53f67dec88cd83d2b8fc2115b7a5d16ba53c1429
Zeyneb Aboelnasr <if22b185@technikum.wien.at>
2023-05-18 20:52:11 +0200

    edit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,5 +29,31 @@ export class LoginComponent {
     console.log('Password: ' + this.password);
     console.log('Confirm Password: ' + this.confirmPassword);
   }
+
+  login() {
+    // Überprüfung des Benutzernamens
+    if (!this.isValidEmail(this.username)) {
+      this.loginError = 'Invalid email address';
+      return;
+    }
+
+    // Überprüfung des Passworts
+    if (this.password !== '12345678') {
+      this.loginError = 'Invalid password';
+      return;
+    }
+
+    // Erfolgreicher Login
+    this.loginError = ''; // Zurücksetzen der Fehlermeldung
+    console.log('Login successful');
+  }
+
+  // Hilfsmethode zur Überprüfung der E-Mail-Adresse
+  private isValidEmail(email: string): boolean {
+    // Implementiere hier die Logik zur Überprüfung der E-Mail-Adresse
+    // Du kannst eine reguläre Ausdruck oder eine andere Methode verwenden
+    // In diesem Beispiel wird eine einfache Überprüfung auf das Vorhandensein eines @-Symbols durchgeführt
+    return email.includes('@');
+  }
   
 }
